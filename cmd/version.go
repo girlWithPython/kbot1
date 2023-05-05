9e06c90cd9808081c6fe2ab7a08cdd5e1f2b97d9
girlWithPython <77209768+girlWithPython@users.noreply.github.com>
2023-05-05 13:54:47 +0000

    real commit

--- cmd/version.go
+++ cmd/version.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -9,14 +8,16 @@ import (
 
 	"github.com/spf13/cobra"
 )
+
+// appVersion Application Version
 var appVersion = "Version"
+
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
-
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/girlWithPython/kbot
 go 1.20
 
 require (
+	github.com/spf13/cobra v1.7.0
+	github.com/stianeikeland/go-rpio v4.2.0+incompatible
+	gopkg.in/telebot.v3 v3.1.3
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
